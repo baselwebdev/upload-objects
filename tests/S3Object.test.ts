55577ab8ef8f28bee8a37aee1a895f7dc2978dcd
Basel <basel@gmx.co.uk>
2020-10-13 23:21:35 +0100

    add test for finding the correct index

--- tests/S3Object.test.ts
+++ tests/S3Object.test.ts
@@ -3,50 +3,24 @@
  * */
 
 import S3Object from '../src/S3Object';
-import S3 from 'aws-sdk/clients/s3';
 import { S3UploaderOptions } from '../S3Uploader';
-import { mocked } from 'ts-jest/utils';
-
-jest.mock('../src/S3Object');
-
-const mockListObjectsV2 = jest.fn(() => {
-    return true;
-});
-
-jest.mock(
-    'aws-sdk/clients/s3',
-    jest.fn(() => {
-        return jest.fn().mockImplementation(() => {
-            return {
-                listObjectsV2: mockListObjectsV2,
-            };
-        });
-    }),
-);
-
-const options: S3UploaderOptions = {
-    bucketName: 'test',
-    objectPrefix: 's3_tester',
-};
 
 describe('S3Object', () => {
-    const mockedS3 = mocked(new S3());
-    let s3: S3Object;
+    it('finds the correct index', () => {
+        const objectPrefix = 's3_tester';
+        const options: S3UploaderOptions = {
+            bucketName: 'test',
+            objectPrefix: 's3_tester',
+        };
 
-    beforeEach(() => {
-        s3 = new S3Object(options);
-        mockedS3.listObjectsV2.mockClear();
-    });
+        const s3 = new S3Object(options);
 
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
+        const data = [
+            {
+                Key: objectPrefix + '_0001/Index.html',
+            },
+        ];
 
-    it('calls constructor once', () => {
-        expect(S3Object).toBeCalledTimes(1);
-    });
-
-    it('returns the correct index value', () => {
-        expect(mockedS3.listObjectsV2()).toEqual(true);
+        expect(s3.getNextIndex(data)).toEqual(2);
     });
 });
