0da1ce3c6ac1237c6b096dd3960f76932e3279a0
Basel <basel@gmx.co.uk>
2020-10-13 23:26:42 +0100

    add test for finding the correct index when some indexes are removed

--- tests/S3Object.test.ts
+++ tests/S3Object.test.ts
@@ -6,15 +6,15 @@ import S3Object from '../src/S3Object';
 import { S3UploaderOptions } from '../S3Uploader';
 
 describe('S3Object', () => {
+    const objectPrefix = 's3_tester';
+    const options: S3UploaderOptions = {
+        bucketName: 'test',
+        objectPrefix: 's3_tester',
+    };
+
+    const s3 = new S3Object(options);
+
     it('finds the correct index', () => {
-        const objectPrefix = 's3_tester';
-        const options: S3UploaderOptions = {
-            bucketName: 'test',
-            objectPrefix: 's3_tester',
-        };
-
-        const s3 = new S3Object(options);
-
         const data = [
             {
                 Key: objectPrefix + '_0001/Index.html',
@@ -22,5 +22,21 @@ describe('S3Object', () => {
         ];
 
         expect(s3.getNextIndex(data)).toEqual(2);
+    });
+
+    it('finds the correct index when some are index are removed', () => {
+        const data = [
+            {
+                Key: objectPrefix + '_0001/Index.html',
+            },
+            {
+                Key: objectPrefix + '_0004/Index.html',
+            },
+            {
+                Key: objectPrefix + '_0007/Index.html',
+            },
+        ];
+
+        expect(s3.getNextIndex(data)).toEqual(8);
     });
 });
