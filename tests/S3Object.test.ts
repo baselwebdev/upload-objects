899d7aa2378d2246f0585fc79294442d11671cd0
Basel <basel@gmx.co.uk>
2020-10-16 17:00:39 +0100

    update tests with the changes made to the classes

--- tests/S3Object.test.ts
+++ tests/S3Object.test.ts
@@ -3,41 +3,36 @@
  * */
 
 import S3Object from '../src/S3Object';
-import { S3UploaderOptions } from '../S3Uploader';
 
 describe('S3Object', () => {
-    const objectPrefix = 's3_tester';
-    const options: S3UploaderOptions = {
-        bucketName: 'test',
-        objectPrefix: 's3_tester',
-    };
+    const objectPrefix = 's3_tester_';
 
-    const s3 = new S3Object(options);
+    const s3 = new S3Object();
 
     it('finds the correct index', () => {
         const data = [
             {
-                Key: objectPrefix + '_0001/Index.html',
+                Key: objectPrefix + '0001/Index.html',
             },
         ];
 
-        expect(s3.getNextIndex(data)).toEqual(2);
+        expect(s3.getNextIndex(data, objectPrefix)).toEqual(2);
     });
 
     it('finds the correct index when some are index are removed', () => {
         const data = [
             {
-                Key: objectPrefix + '_0001/Index.html',
+                Key: objectPrefix + '0001/Index.html',
             },
             {
-                Key: objectPrefix + '_0004/Index.html',
+                Key: objectPrefix + '0004/Index.html',
             },
             {
-                Key: objectPrefix + '_0007/Index.html',
+                Key: objectPrefix + '0007/Index.html',
             },
         ];
 
-        expect(s3.getNextIndex(data)).toEqual(8);
+        expect(s3.getNextIndex(data, objectPrefix)).toEqual(8);
     });
 
     it('turns string into numbers', () => {
